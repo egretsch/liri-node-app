fd209628f528c89eacfbc871c11463d060f1f606
Edward <edwardgretsch@gmail.com>
2018-07-10 22:11:09 -0700

    update 3

--- liri.js
+++ liri.js
@@ -8,27 +8,81 @@ const fs = require('fs');
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-function getTweets(userName) {
 
-    console.log("function called")
-    
+const commad = process.argv[2];
+const movOrSon = process.argv;
 
-var params = { screen_name: userName };
-client.get('statuses/user_timeline', params, function (error, tweets, response) {
-    if (error) {
-        console.log(error);
+//movie or song
+var x = "";
+//attaches multiple word arguments
+for (var i = 3; i < movOrSon.length; i++) {
+    if (i > 3 && i < movOrSon.length) {
+        x = x + "+" + movOrSon[i];
+    } else {
+        x = x + movOrSon[i];
     }
-    else{
-        for (let i = 0; i < tweets.length; i++) {
-            let tweet = tweets[i].text;
-            let timeStamp = tweets[i].created_at;
-
-            console.log(tweet, timeStamp);
-            
-        }
-    }
-});
 }
 
 
-getTweets("edward_gretsch");
+switch (commad) {
+    case "my-tweets": getTweets("edward_gretsch");
+        break;
+    case "spotify-this-song": 
+        if (x) {
+            getSpotify(x);
+        } else {
+            getSpotify("The Sign");
+        }
+        break;
+    case "movie-this": console.log(num1 * num2);
+        break;
+    case "do-what-it-says": console.log(num1 / num2);
+        break;
+    default: console.log("!!!You Got Nothing!!!");
+        break;
+}
+
+function getTweets(userName) {
+    var params = { screen_name: userName };
+    client.get('statuses/user_timeline', params, function (error, tweets) {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            for (let i = 0; i < tweets.length; i++) {
+                let tweet = tweets[i].text;
+                var date = tweets[i].created_at;
+                console.log("@edward_gretsch: " + tweet + " Created At: " + date.substring(0, 19));
+                console.log("-----------------------");
+                //adds text to log.txt file
+                fs.appendFile('log.txt', "@edward_gretsch: " + tweet + " Created At: " + date.substring(0, 19));
+                fs.appendFile('log.txt', "-----------------------");
+            }
+        }
+    });
+}
+
+function getSpotify(song) {
+    spotify.search({ type: 'track', query: song }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        } else {
+            var songData = data.tracks.items[i];
+            const artist = songData.artists[0].name;
+            const song = songData.name;
+            const songURL = songData.preview_url;
+            const album = songData.album.name
+            console.log(album);
+            console.log("Artist: " + artist);
+            console.log("Song Name: " + song);
+            console.log("-----------------------");
+
+            fs.appendFile('log.txt', "@edward_gretsch: " + tweet + " Created At: " + date.substring(0, 19));
+            fs.appendFile('log.txt', "-----------------------");
+        }
+    });
+
+}
+
+
+
